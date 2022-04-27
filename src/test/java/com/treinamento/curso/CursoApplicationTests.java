ec28f1d6b8dd623cc177e598acd542f16a7f1e01
Joaquim.Neto <engjoaquimnetoo@hotmail.com>
2022-04-27 10:52:08 -0300

    Projeto criado

---
+++ src/test/java/com/treinamento/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
